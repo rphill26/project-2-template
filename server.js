b0990c873a77492350f69f8608ca0ec829f5fc8b
Randall Phillips <rphill45@uncc.edu>
2019-11-26 13:20:22 -0500

    roughed out minimal server and setup start script

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log( `==> Server listening at ${PORT}` );
+});
+
