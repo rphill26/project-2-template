e3ed591aec05de556428adb37eb2d827f3c2389c
Randall Phillips <rphill45@uncc.edu>
2019-11-26 13:34:16 -0500

    added more server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+//View Engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(PORT, () => {
     console.log( `==> Server listening at ${PORT}` );
